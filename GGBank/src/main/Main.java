3da0e0502df8848697a153be656f27c6a3018f9c
Guichard Gaël <g.guichard22@gmail.com>
2023-05-10 10:52:05 +0200

    Update the "Main" class.

--- GGBank/src/main/Main.java
+++ GGBank/src/main/Main.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import components.Client;
+import components.Account;
+import components.CurrentAccount;
+import components.SavingsAccount;
 
 //1.1.1 Creation of the Main class
 public class Main {
@@ -11,6 +14,9 @@ public class Main {
 	public static void main(String[] args) {
 		List<Client> clientCollection = loadClientsCollection(3);
 		displayAllClients(clientCollection);
+		
+		List<Account> accountCollection = loadAccountsCollection(clientCollection);
+		displayAllClientAccounts(accountCollection);
 	}
 	
 	/**
@@ -35,5 +41,33 @@ public class Main {
 	 */
 	public static void displayAllClients(List<Client> arrayToDisplay) {
 		arrayToDisplay.forEach(client -> System.out.println(client));
+	}
+	
+	/**
+	 * Create and return a collection with instances of the CurrentAccount and SavingsAccount classes for each client
+	 * @param clients Collection of clients
+	 */
+	public static List<Account> loadAccountsCollection(List<Client> clients) {
+		
+		List<Account> newAccountCollection = new ArrayList<>();
+		
+		clients.forEach( client -> {
+			SavingsAccount newSavingsAccount = new SavingsAccount("general", client);
+			CurrentAccount newCurrentAccount = new CurrentAccount("general", client);
+			
+			newAccountCollection.add(newSavingsAccount);
+			newAccountCollection.add(newCurrentAccount);
+		});
+
+		
+		return newAccountCollection;
+	}
+	
+	/**
+	 * Display all instances of the collection
+	 * @param arrayToDisplay The collection to display
+	 */
+	public static void displayAllClientAccounts(List<Account> arrayToDisplay) {
+		arrayToDisplay.forEach(account -> System.out.println(account));
 	}
 }
